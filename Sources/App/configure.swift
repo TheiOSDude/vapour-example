aff72a886f8f14e935f3f4871ce951d6b45d5ed4
Lee Burrows <>
2021-05-16 21:11:24 +0100

    Example Start

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,6 +5,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    app.http.server.configuration.port = 8081
     // register routes
     try routes(app)
 }
